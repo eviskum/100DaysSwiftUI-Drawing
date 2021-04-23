414dc2b0c0296674df92a03315e9a7ec8d13b184
Esben Viskum <viskum@viskum.dk>
2021-04-23 11:17:36 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Esben Viskum on 23/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
