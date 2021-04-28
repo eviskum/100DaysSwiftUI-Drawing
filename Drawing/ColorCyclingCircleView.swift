6ceef5f40710e585ed0e1f7be24bd2e1268d12f9
Esben Viskum <viskum@viskum.dk>
2021-04-28 11:08:37 +0200

    Finished project

---
+++ Drawing/ColorCyclingCircleView.swift
@@ -0,0 +1,38 @@
+//
+//  ColorCyclingCircleView.swift
+//  Drawing
+//
+//  Created by Esben Viskum on 27/04/2021.
+//
+
+import SwiftUI
+
+struct ColorCyclingCircleView: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 { targetHue -= 1 }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ColorCyclingCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCyclingCircleView()
+    }
+}
--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,10 +7,377 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationsAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationsAdjustment
+        let modifiedEnd = endAngle - rotationsAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct Checkboard : Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row+column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 { path.move(to: CGPoint(x: x, y: y)) }
+            else { path.addLine(to: CGPoint(x: x, y: y ))}
+        }
+        
+        return path
+    }
+}
+
+struct Arrow: Shape {
+    var arrowWidth = CGFloat(10)
+    
+    var animatableData: CGFloat {
+        get { arrowWidth }
+        set { self.arrowWidth = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let arrowStartX = rect.midX - CGFloat(arrowWidth / 2)
+        let arrowStartY = CGFloat(rect.midY)
+
+        let arrowLine = CGRect(x: arrowStartX, y: arrowStartY, width: arrowWidth, height: rect.height / 2)
+
+        path.addRect(arrowLine)
+
+        path.move(to: CGPoint(x: rect.midX, y: 0))
+        path.addLine(to: CGPoint(x: rect.midX + rect.width / 6, y: rect.height / 2))
+        path.addLine(to: CGPoint(x: rect.midX - rect.width / 6, y: rect.height / 2))
+        path.addLine(to: CGPoint(x: rect.midX, y: 0))
+        
+        return path
+    }
+}
+
+
 struct ContentView: View {
+/*    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0 */
+    @State private var colorCycle = 0.0
+//    @State private var amount: CGFloat = 0.0
+//    @State private var insetAmount: CGFloat = 50
+//    @State private var rows = 4
+//    @State private var columns = 4
+/*    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6 */
+    @State private var arrowWidth = CGFloat(10.0)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+/*        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)) */
+        
+/*        Triangle()
+//            .fill(Color.red)
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300) */
+        
+/*        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(Color.blue, lineWidth: 10)
+            .frame(width: 300, height: 300) */
+        
+/*        Circle()
+            .strokeBorder(Color.blue, lineWidth: 40) */
+        
+/*        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40) */
+
+/*        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                .stroke(Color.red, lineWidth: 1)
+                .fill(Color.red, style: FillStyle(eoFill: true, antialiased: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        } */
+        
+/*        Text("Hello world")
+            .frame(width: 300, height: 300)
+//            .background(Image("Esben"))
+//            .border(Image("Esben"), width: 10)
+            .border(ImagePaint(image: Image("Esben"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30) */
+        
+/*        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Esben"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200) */
+        
+/*        VStack {
+            ColorCyclingCircleView(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            Slider(value: $colorCycle)
+        } */
+        
+/*        ZStack {
+            Image("Esben")
+            
+            Rectangle()
+                .fill(Color.red)
+                .blendMode(.multiply)
+        }
+        .frame(width: 400, height: 500)
+        .clipped() */
+        
+ //       Image("Esben")
+ //           .colorMultiply(.blue)
+        
+/*        VStack {
+            ZStack {
+                Circle()
+//                    .fill(Color.red)
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -50)
+                    .blendMode(.screen)
+                Circle()
+//                    .fill(Color.green)
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                Circle()
+//                    .fill(Color.blue)
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all) */
+        
+/*        VStack {
+            Image("Esben")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(Double(amount))
+                .blur(radius: (1-amount) * 20)
+            Slider(value: $amount)
+                .padding()
+        } */
+ 
+/*        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            } */
+        
+/*        Checkboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
+            } */
+        
+/*        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
+        } */
+        
+/*        VStack {
+            Arrow(arrowWidth: arrowWidth)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    withAnimation {
+                        self.arrowWidth = CGFloat.random(in: 1...50)
+                    }
+                }
+            
+//            Slider(value: $arrowWidth, in: 1...50, step: 1)
+//                .padding([.horizontal, .bottom])
+        } */
+
+        VStack {
+            ColorCyclingRectangleView(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            Slider(value: $colorCycle)
+        }
+
+
     }
 }
 
